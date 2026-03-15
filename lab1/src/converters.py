05fdda7a45980f473765c9e20a5efda604fb2680
Артём Перфоратор <artemmarudin1@gmail.com>
2026-03-15 21:05:06 +0300

    Update converters.py

--- lab1/src/converters.py
+++ lab1/src/converters.py
@@ -1,89 +1,103 @@
 from src.bit_array import BitArray
 
+WORD_SIZE = 32
+MAX_BITS_INDEX = WORD_SIZE - 1
+MIN_INT32 = -2147483648
+FIXED_INT_BITS = 14
+FIXED_FRAC_START = 15
+
 class NumberConverter:
-    def decimal_to_direct(self, val: int) -> BitArray:
-        b = BitArray()
-        b.bits[0] = 1 if val < 0 else 0
-        temp = abs(int(val))
-        for i in range(31, 0, -1):
-            b.bits[i] = temp % 2
-            temp //= 2
-        return b
-
-    def decimal_to_reverse(self, val: int) -> BitArray:
-        b = self.decimal_to_direct(val)
-        if val < 0:
-            for i in range(1, 32):
-                b.bits[i] = 1 - b.bits[i]
-        return b
-
-    def decimal_to_additional(self, val: int) -> BitArray:
-        if val == -2147483648:
-            b = BitArray()
-            b.bits[0] = 1
-            return b
-            
-        b = self.decimal_to_reverse(val)
-        if val < 0:
-            c = 1
-            for i in range(31, 0, -1):
-                t = b.bits[i] + c
-                b.bits[i] = t % 2
-                c = t // 2
-        return b
-
-    def direct_to_decimal(self, b: BitArray) -> int:
-        val = 0
-        for i in range(1, 32):
-            val = val * 2 + b.bits[i]
-        return -val if b.bits[0] == 1 else val
-
-    def additional_to_decimal(self, b: BitArray) -> int:
-        if b.bits == [1] + [0] * 31:
-            return -2147483648
-            
-        is_n = b.bits[0] == 1
-        t = b.bits.copy()
+    def decimal_to_direct(self, decimal_value: int) -> BitArray:
+        result_array = BitArray()
+        result_array.bits[0] = 1 if decimal_value < 0 else 0
+        absolute_value = abs(int(decimal_value))
         
-        if is_n:
-            c = 1
-            for i in range(31, 0, -1):
-                diff = t[i] - c
-                if diff < 0:
-                    t[i] = 1
-                    c = 1
+        for bit_index in range(MAX_BITS_INDEX, 0, -1):
+            result_array.bits[bit_index] = absolute_value % 2
+            absolute_value //= 2
+            
+        return result_array
+
+    def decimal_to_reverse(self, decimal_value: int) -> BitArray:
+        result_array = self.decimal_to_direct(decimal_value)
+        if decimal_value < 0:
+            for bit_index in range(1, WORD_SIZE):
+                result_array.bits[bit_index] = 1 - result_array.bits[bit_index]
+        return result_array
+
+    def decimal_to_additional(self, decimal_value: int) -> BitArray:
+        if decimal_value == MIN_INT32:
+            result_array = BitArray()
+            result_array.bits[0] = 1
+            return result_array
+            
+        result_array = self.decimal_to_reverse(decimal_value)
+        if decimal_value < 0:
+            carry_bit = 1
+            for bit_index in range(MAX_BITS_INDEX, 0, -1):
+                temp_sum = result_array.bits[bit_index] + carry_bit
+                result_array.bits[bit_index] = temp_sum % 2
+                carry_bit = temp_sum // 2
+        return result_array
+
+    def direct_to_decimal(self, bit_array: BitArray) -> int:
+        decimal_value = 0
+        for bit_index in range(1, WORD_SIZE):
+            decimal_value = decimal_value * 2 + bit_array.bits[bit_index]
+        return -decimal_value if bit_array.bits[0] == 1 else decimal_value
+
+    def additional_to_decimal(self, bit_array: BitArray) -> int:
+        if bit_array.bits == [1] + [0] * MAX_BITS_INDEX:
+            return MIN_INT32
+            
+        is_negative = bit_array.bits[0] == 1
+        temp_bits = bit_array.bits.copy()
+        
+        if is_negative:
+            borrow_bit = 1
+            for bit_index in range(MAX_BITS_INDEX, 0, -1):
+                difference = temp_bits[bit_index] - borrow_bit
+                if difference < 0:
+                    temp_bits[bit_index] = 1
+                    borrow_bit = 1
                 else:
-                    t[i] = diff
-                    c = 0
-            for i in range(1, 32):
-                t[i] = 1 - t[i]
+                    temp_bits[bit_index] = difference
+                    borrow_bit = 0
+                    
+            for bit_index in range(1, WORD_SIZE):
+                temp_bits[bit_index] = 1 - temp_bits[bit_index]
                 
-        val = 0
-        for i in range(1, 32):
-            val = val * 2 + t[i]
-        return -val if is_n else val
+        decimal_value = 0
+        for bit_index in range(1, WORD_SIZE):
+            decimal_value = decimal_value * 2 + temp_bits[bit_index]
+        return -decimal_value if is_negative else decimal_value
 
-    def decimal_to_fixed(self, val: float) -> BitArray:
-        b = BitArray()
-        b.bits[0] = 1 if val < 0 else 0
-        v = abs(val)
-        i_part = int(v)
-        f_part = v - i_part
-        for i in range(14, 0, -1):
-            b.bits[i] = i_part % 2
-            i_part //= 2
-        for i in range(15, 32):
-            f_part *= 2
-            b.bits[i] = int(f_part)
-            f_part -= int(f_part)
-        return b
+    def decimal_to_fixed(self, float_value: float) -> BitArray:
+        result_array = BitArray()
+        result_array.bits[0] = 1 if float_value < 0 else 0
+        absolute_value = abs(float_value)
+        integer_part = int(absolute_value)
+        fractional_part = absolute_value - integer_part
+        
+        for bit_index in range(FIXED_INT_BITS, 0, -1):
+            result_array.bits[bit_index] = integer_part % 2
+            integer_part //= 2
+            
+        for bit_index in range(FIXED_FRAC_START, WORD_SIZE):
+            fractional_part *= 2
+            result_array.bits[bit_index] = int(fractional_part)
+            fractional_part -= int(fractional_part)
+            
+        return result_array
 
-    def fixed_to_decimal(self, b: BitArray) -> float:
-        i_part = 0
-        for i in range(1, 15):
-            i_part = i_part * 2 + b.bits[i]
-        f_part = 0
-        for i in range(15, 32):
-            f_part += b.bits[i] * (2 ** -(i - 14))
-        res = round(i_part + f_part, 5)
-        return -res if b.bits[0] == 1 else res
+    def fixed_to_decimal(self, bit_array: BitArray) -> float:
+        integer_part = 0
+        for bit_index in range(1, FIXED_FRAC_START):
+            integer_part = integer_part * 2 + bit_array.bits[bit_index]
+            
+        fractional_part = 0
+        for bit_index in range(FIXED_FRAC_START, WORD_SIZE):
+            fractional_part += bit_array.bits[bit_index] * (2 ** -(bit_index - FIXED_INT_BITS))
+            
+        final_result = round(integer_part + fractional_part, 5)
+        return -final_result if bit_array.bits[0] == 1 else final_result
